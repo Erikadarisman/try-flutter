2d530bcfbd70405d3c7eba74e1f1632dee1aaebc
Erik <erikadarisman@gmail.com>
2020-01-23 11:49:18 +0700

    nav component / appbar

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  void Test(){
+    print("testing");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,24 +73,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Container(
-          child: GestureDetector(
-            onPanStart: (DragStartDetails details){
-              print("start");
-              print(details);
-            },
-            onPanUpdate: (DragUpdateDetails details){
-              print("Update");
-              print(details);
-            },
-            child: new Image(image: AssetImage("assets/image/download.jpeg")),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            tooltip: "Test",
+            onPressed: Test,
           )
-        ),
+        ],
       ),
+
+      body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
